83424dbe15e2b6953c88e5babeda4c69e5516a9f
Rizdvo <56375799+Rizdvo@users.noreply.github.com>
2020-03-12 09:32:22 -0700

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    public class Book
+    {
+        public Book(string name)
+        {
+            grades = new List<double>();
+            Name = name;
+        }
+
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+
+                case 'B':
+                    AddGrade(80);
+                    break;
+
+                case 'C':
+                    AddGrade(70);
+                    break;
+
+                default:
+                    AddGrade(0);
+                    break;
+
+            }
+        }
+
+
+
+
+        public void AddGrade(double grade)
+        {
+            if (grade <= 100 && grade >=0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                Console.WriteLine("Invalid value");
+            }
+        }
+
+        public Statistics GetStatistics()
+        {
+            var result = new Statistics();
+            result.Average = 0.0;
+            result.High = double.MinValue;
+            result.Low = double.MaxValue;
+
+            for(var index =0; index < grades.Count; index +=1)
+            {
+                result.Low = Math.Min(grades[index], result.Low);
+                result.High = Math.Max(grades[index], result.High);
+                result.Average += grades[index];
+            }
+            result.Average /= grades.Count;
+
+            return result;
+        }
+
+        private List<double> grades;
+        public string Name;
+
+    }
+}
