f19bba184143e96ff10084db2aa814684745d137
Rizdvo <56375799+Rizdvo@users.noreply.github.com>
2020-03-12 16:06:05 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Sasha's Grade Book");
+            book.GradeAdded += OnGradeAdded;
+                                        
+            while (true)
+            {
+                Console.WriteLine("Please, enter grade or 'q' to quit: ");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+
+            var stats = book.GetStatistics();
+         
+            Console.WriteLine($"For the book named {book.Name}");
+            Console.WriteLine($"The lowes grade is {stats.Low}");
+            Console.WriteLine($"The Highest grade is {stats.High}");
+            Console.WriteLine($"The avarage grade is {stats.Average:N1}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+
+        static void OnGradeAdded(object sender, EventArgs e)
+        {
+            Console.WriteLine("A grade was added");
+        }
+
+    }
+}
